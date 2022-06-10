dd00e9ba8940a177d3529db8439f92312d2888eb
Fernando Zanutto <ferzanutto1999@gmail.com>
2022-06-09 21:00:20 -0300

    Continuação curso TDD

--- app/src/main/java/br/com/alura/leilao/model/Leilao.java
+++ app/src/main/java/br/com/alura/leilao/model/Leilao.java
@@ -2,7 +2,6 @@ package br.com.alura.leilao.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,8 +9,8 @@ public class Leilao implements Serializable {
 
     private final String descricao;
     private final List<Lance> lances;
-    private double maiorLance = Double.NEGATIVE_INFINITY;
-    private double menorLance = Double.POSITIVE_INFINITY;
+    private double maiorLance = 0.0;
+    private double menorLance = 0.0;
 
     public Leilao(String descricao) {
         this.descricao = descricao;
@@ -19,11 +18,65 @@ public class Leilao implements Serializable {
     }
 
     public void proporLance(Lance lance) {
+        if (lanceNaoValido(lance)) return;
+
         lances.add(lance);
-        Collections.sort(lances);
+
         double valorLance = lance.getValor();
+
+        if (definePrimeiroLance(valorLance)) return;
+
+        Collections.sort(lances);
+
         calcularMaiorLance(valorLance);
         calcularMenorLance(valorLance);
+    }
+
+    private boolean definePrimeiroLance(double valorLance) {
+        if (lances.size() == 1) {
+            maiorLance = valorLance;
+            menorLance = valorLance;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean lanceNaoValido(Lance lance) {
+        double valorLance = lance.getValor();
+
+        if (lanceMenorQueOUltimo(valorLance)) throw new RuntimeException("Lance foi menor que o maior lance");
+
+        if (!lances.isEmpty()) {
+            Usuario usuarioNovo = lance.getUsuario();
+            if (usuarioMesmoQueUltimoLance(usuarioNovo)) throw new RuntimeException("Mesmo usuário do último lance");
+
+            if (cincoLancesDoUsuario(usuarioNovo)) {
+                throw new RuntimeException("Usuário já deu cinco lances");
+            }
+        }
+        return false;
+    }
+
+    private boolean cincoLancesDoUsuario(Usuario usuarioNovo) {
+        int lancesDoUsuario = 0;
+        for (Lance l : lances) {
+            Usuario usuarioExistente = l.getUsuario();
+            if (usuarioExistente.equals(usuarioNovo)) {
+                lancesDoUsuario++;
+                if (lancesDoUsuario == 5) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean usuarioMesmoQueUltimoLance(Usuario usuarioNovo) {
+        Usuario ultimoUsuario = lances.get(0).getUsuario();
+
+        return usuarioNovo.equals(ultimoUsuario);
+    }
+
+    private boolean lanceMenorQueOUltimo(double valorLance) {
+        return maiorLance > valorLance;
     }
 
     private void calcularMenorLance(double valorLance) {
@@ -53,5 +106,9 @@ public class Leilao implements Serializable {
     public List<Lance> getTresMaioresLances() {
         int endIndex = Math.min(3, lances.size());
         return lances.subList(0, endIndex);
+    }
+
+    public int getQuantidadeLances() {
+        return lances.size();
     }
 }
--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -1,21 +1,27 @@
 package br.com.alura.leilao.model;
 
-import junit.framework.TestCase;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
 
 import java.util.List;
 
-public class LeilaoTest extends TestCase {
+public class LeilaoTest {
 
     public static final double DELTA = 0.0001;
     private final Leilao console = new Leilao("Console");
     private final Usuario usuarioFernando = new Usuario("Fernando");
 
+    @Test
     public void test_DevolveDescricao_QuandoRecebeDescricao() {
         String descricaoDevolvida = console.getDescricao();
 
         assertEquals("Console", descricaoDevolvida);
     }
 
+    @Test
     public void test_DevolveMaiorLance_QuandoRecebeApenasUmLance() {
         console.proporLance(new Lance(usuarioFernando, 200.0));
 
@@ -24,6 +30,7 @@ public class LeilaoTest extends TestCase {
         assertEquals(200.0, lanceDevolvidoConsole, DELTA);
     }
 
+    @Test
     public void test_DevolveMaiorLance_QuandoRecebeLancesOrdemCrescente() {
         Usuario usuarioGiovanna = new Usuario("Giovanna");
 
@@ -34,16 +41,18 @@ public class LeilaoTest extends TestCase {
         assertEquals(200.0, lanceDevolvidoComputador, DELTA);
     }
 
+    @Test
     public void test_DevolveMaiorLance_QUandoRecebeLancesOrdemDecrescente() {
         Usuario usuarioGiovanna = new Usuario("Giovanna");
 
-        console.proporLance(new Lance(usuarioFernando, 500.0));
         console.proporLance(new Lance(usuarioGiovanna, 200.0));
+        console.proporLance(new Lance(usuarioFernando, 500.0));
 
         double lanceDevolvidoCarro = console.getMaiorLance();
         assertEquals(500.0, lanceDevolvidoCarro, DELTA);
     }
 
+    @Test
     public void test_DevolveMenorLance_QuandoRecebeApenasUmLance() {
         console.proporLance(new Lance(usuarioFernando, 200.0));
 
@@ -52,6 +61,7 @@ public class LeilaoTest extends TestCase {
         assertEquals(200.0, lanceDevolvidoConsole, DELTA);
     }
 
+    @Test
     public void test_DevolveMenorLance_QuandoRecebeLancesOrdemCrescente() {
         Usuario usuarioGiovanna = new Usuario("Giovanna");
 
@@ -62,21 +72,11 @@ public class LeilaoTest extends TestCase {
         assertEquals(100.0, lanceDevolvidoComputador, DELTA);
     }
 
-    public void test_DevolveMenorLance_QuandoRecebeLancesOrdemDecrescente() {
-        Usuario usuarioGiovanna = new Usuario("Giovanna");
-
-        console.proporLance(new Lance(usuarioFernando, 500.0));
-        console.proporLance(new Lance(usuarioGiovanna, 200.0));
-
-        double lanceDevolvidoCarro = console.getMenorLance();
-        assertEquals(200.0, lanceDevolvidoCarro, DELTA);
-    }
-
+    @Test
     public void test_DevolveTresMaioresLances_QuandoRecebeTresLances() {
         console.proporLance(new Lance(usuarioFernando, 200.0));
-        console.proporLance(new Lance(usuarioFernando, 400.0));
-        console.proporLance(new Lance(usuarioFernando, 300.0));
-        console.proporLance(new Lance(usuarioFernando, 150.0));
+        console.proporLance(new Lance(new Usuario("Giovanna"), 300.0));
+        console.proporLance(new Lance(new Usuario("Ganache"), 400.0));
 
         List<Lance> maioresLances = console.getTresMaioresLances();
 
@@ -87,12 +87,14 @@ public class LeilaoTest extends TestCase {
     }
 
 
+    @Test
     public void test_DevolveTresMaioresLances_QuandoNaoRecebeLances() {
         List<Lance> maioresLances = console.getTresMaioresLances();
 
         assertEquals(0, maioresLances.size());
     }
 
+    @Test
     public void test_DevolveTresMaioresLances_QuandoRecebeUmLance() {
         console.proporLance(new Lance(usuarioFernando, 200.0));
         List<Lance> maioresLances = console.getTresMaioresLances();
@@ -101,16 +103,74 @@ public class LeilaoTest extends TestCase {
         assertEquals(200.0, maioresLances.get(0).getValor(), DELTA);
     }
 
+    @Test
     public void test_DevolveTresMaioresLances_QuandoRecebeQuatroLances() {
         console.proporLance(new Lance(usuarioFernando, 250.0));
-        console.proporLance(new Lance(usuarioFernando, 270.0));
-        console.proporLance(new Lance(usuarioFernando, 500.0));
-        console.proporLance(new Lance(usuarioFernando, 290.0));
+        console.proporLance(new Lance(new Usuario("Giovanna"), 270.0));
+        console.proporLance(new Lance(new Usuario("Ganache"), 290.0));
+        console.proporLance(new Lance(new Usuario("Jorge, O Rato"), 500.0));
         List<Lance> maioresLances = console.getTresMaioresLances();
 
         assertEquals(3, maioresLances.size());
         assertEquals(500.0, maioresLances.get(0).getValor(), DELTA);
         assertEquals(290.0, maioresLances.get(1).getValor(), DELTA);
         assertEquals(270.0, maioresLances.get(2).getValor(), DELTA);
+    }
+
+    @Test
+    public void test_DevolveValorZeraParaMaiorLance_QuandoNaoTiverLance() {
+        double maiorLance = console.getMaiorLance();
+        assertEquals(0.0, maiorLance, DELTA);
+    }
+
+    @Test
+    public void test_DevolveValorZeraParaMenorLance_QuandoNaoTiverLance() {
+        double menorLance = console.getMenorLance();
+        assertEquals(0.0, menorLance, DELTA);
+    }
+
+    @Test
+    public void test_naoDeveAdicionarLance_QuandoForMenorQueMaiorLance() {
+        console.proporLance(new Lance(usuarioFernando, 500.0));
+
+        try {
+            console.proporLance(new Lance(new Usuario("Giovanna"), 400.0));
+            fail("Era esperado RuntimeException");
+        } catch (RuntimeException exception) {
+            assertEquals("Lance foi menor que o maior lance", exception.getMessage());
+        }
+    }
+
+    @Test
+    public void naoDeveAdicinarLance_QuandoForMesmoUsuarioDoUltimoLance() {
+        console.proporLance(new Lance(usuarioFernando, 200.0));
+
+        try {
+            console.proporLance(new Lance(usuarioFernando, 300.0));
+            fail("Era esperado RuntimeException");
+        } catch (RuntimeException exception) {
+            assertEquals("Mesmo usuário do último lance", exception.getMessage());
+        }
+    }
+
+    @Test
+    public void naoDeveAdicionarLance_QuandoUsuarioJaTiverCincoLances() {
+        Usuario usuarioGiovanna = new Usuario("Giovanna");
+        console.proporLance(new Lance(usuarioFernando, 200.0));
+        console.proporLance(new Lance(usuarioGiovanna, 300.0));
+        console.proporLance(new Lance(usuarioFernando, 400.0));
+        console.proporLance(new Lance(usuarioGiovanna, 500.0));
+        console.proporLance(new Lance(usuarioFernando, 600.0));
+        console.proporLance(new Lance(usuarioGiovanna, 700.0));
+        console.proporLance(new Lance(usuarioFernando, 800.0));
+        console.proporLance(new Lance(usuarioGiovanna, 900.0));
+        console.proporLance(new Lance(usuarioFernando, 1000.0));
+        console.proporLance(new Lance(usuarioGiovanna, 1100.0));
+
+        try {
+            console.proporLance(new Lance(usuarioFernando, 1200.0));
+        } catch (RuntimeException e) {
+            assertEquals("Usuário já deu cinco lances", e.getMessage());
+        }
     }
 }
